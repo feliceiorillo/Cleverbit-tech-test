6553b3025d7667628bace809f1a4a2c387ba16d6
Felice Iorillo <felice.iorillo@outlook.it>
2021-01-12 16:05:50 +0100

    Add project files.

---
+++ Cleverbit-tech-test/Controllers/PingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cleverbit_tech_test.Controllers
+{
+   
+    public class PingController : BaseAPIController
+    {
+        private readonly IWebHostEnvironment _env;
+        public PingController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok($"I am working on the: {_env.EnvironmentName}");
+        }
+    }
+}
